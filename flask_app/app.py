ec25140ef5b1c6e31d3d86e4e5023a48b20f93e0
Thomas Garver <tgarv@mit.edu>
2017-07-25 18:46:56 +0000

    Request validation and documentation

--- flask_app/app.py
+++ flask_app/app.py
@@ -17,28 +17,39 @@ def create_app(settings_key='dev'):
     def create_log(app_id):
         request_json = request.get_json()
         if not request_json:
+            # Make sure a JSON object was provided with the request
             return 'Malformed request', 400
-        request_start = request_json.get('start')
-        request_end = request_json.get('end')
+
+        request_start = request_json.get('start', -1)
+        request_end = request_json.get('end', -1)
+        if not request_start and not request_end:
+            # Make sure start and end are set. We could do additional validation,
+            # but those are the only fields that are strictly required.
+            return 'Malformed request -- please provide a "start" and "end"', 400
+
         if app_id not in app.logs:
+            # We don't have a tree yet for this app ID, so create one
             app.logs[app_id] = IntervalTree()
         app.logs[app_id].addi(request_start, request_end, request_json)
-        return 'Hello, ' + str(app_id)
+
+        return '', 200
+
 
     @app.route('/<app_id>', methods=['GET'])
     def get_logs(app_id):
-        if not request.args:
-            return 'Malformed request', 400
-        logs_for_app = app.logs.get(app_id, [])
-        if logs_for_app:
-            request_start = request.args.get('start')
-            request_end = request.args.get('end')
-            logs_in_range = logs_for_app.search(int(request_start), int(request_end))
-            logs_in_range = [log.data for log in logs_in_range]
-        else:
-            logs_in_range = []
+        required_parameters = set(['start', 'end'])
+        if set(request.args.viewkeys()) < required_parameters:
+            # A quick way to check that all the required arguments have been provided
+            # (via https://stackoverflow.com/a/1285926)
+            return 'Malformed request -- please provide a "start" and "end"', 400
 
-        return json.dumps({'logs': logs_in_range})
+        logs_for_app = app.logs.get(app_id, [])
+        logs_in_range = []
+        if logs_for_app:
+            logs_in_range = logs_for_app.search(int(request.args.get('start')), int(request.args.get('end')))
+            logs_in_range = [log.data for log in logs_in_range]
+
+        return json.dumps(logs_in_range)
 
     return app
 
