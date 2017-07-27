b7fbe250cf0ee4b403d8ab270abbff2353dad509
Thomas Garver <tgarv@mit.edu>
2017-07-27 13:32:10 +0000

    Add more documentation and README

--- flask_app/app.py
+++ flask_app/app.py
@@ -6,8 +6,7 @@ import json
 def create_app(settings_key='dev'):
     app = Flask(__name__)
     app.debug = True
-    app.logs = {}
-    # app.config.from_object('config')
+    app.logs = {}  # This is a dictionary mapping app ID -> IntervalTree, holding the Interval Tree for each app ID.
 
     @app.route('/')
     def main():
@@ -17,11 +16,11 @@ def create_app(settings_key='dev'):
     def create_log(app_id):
         request_json = request.get_json()
         if not request_json:
-            # Make sure a JSON object was provided with the request
+            # we need a JSON object for this request
             return 'Malformed request', 400
 
-        request_start = request_json.get('start', -1)
-        request_end = request_json.get('end', -1)
+        request_start = request_json.get('start')
+        request_end = request_json.get('end')
         if not request_start and not request_end:
             # Make sure start and end are set. We could do additional validation,
             # but those are the only fields that are strictly required.
@@ -30,8 +29,12 @@ def create_app(settings_key='dev'):
         if app_id not in app.logs:
             # We don't have a tree yet for this app ID, so create one
             app.logs[app_id] = IntervalTree()
+
+        # Add the JSON object in the interval (start, end)
+        # https://pypi.python.org/pypi/intervaltree
         app.logs[app_id].addi(request_start, request_end, request_json)
 
+        # No response payload required, just a 200 response
         return '', 200
 
 
@@ -47,14 +50,15 @@ def create_app(settings_key='dev'):
         logs_in_range = []
         if logs_for_app:
             logs_in_range = logs_for_app.search(int(request.args.get('start')), int(request.args.get('end')))
+            # There's some extra data about the interval that gets stored in the tree, so return just a list of the data
             logs_in_range = [log.data for log in logs_in_range]
 
         return json.dumps(logs_in_range)
 
     return app
 
+
 if __name__ == '__main__':
     app = create_app()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)  # Use this for an externally-available app
-    # app.run()
+    port = int(os.environ.get('PORT', 3000))
+    app.run(port=port)
